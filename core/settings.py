6d50d9db8bbde248ba07d1838d7c7594e63db5c0
Andrii <andremailboxua@gmail.com>
2023-06-25 17:50:42 +0300

    Setting

--- core/settings.py
+++ core/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media_content/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
